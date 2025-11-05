827f6a4cb5bba679cb17a6785003592b47fb1d8a
Tiago Salgado <tiago.salgado@devsoftbr.com>
2025-11-05 14:33:07 -0300

    Install clarity

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import { Providers } from "./providers";
 import { Router } from "./routes/router";
 import "@/style/globalStyle.css";
 import { Toaster } from "./components/ui/sonner";
+import Clarity from "@microsoft/clarity";
+
+const projectId = import.meta.env.VITE_CLARITY;
+
+if (projectId) Clarity.init(projectId);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
