990b9b8a5cc8b2622c835bb32885c93e428495a7
Tiago Salgado <tiago.salgado@devsoftbr.com>
2026-03-23 10:58:18 -0300

    Melhoria na visualização do item no kanban

--- src/pages/dashboard/whatsapp/contact/whatsappKanban/whatsappKanbanItem.tsx
+++ src/pages/dashboard/whatsapp/contact/whatsappKanban/whatsappKanbanItem.tsx
@@ -4,6 +4,7 @@ import { type DetailedHTMLProps, type FC } from "react";
 import { WhatsappChatContactMessage } from "../../components/whatsappChatContactMessage";
 import { WhatsappMessageType } from "@/enums/whatsappMessageType.enum";
 import { cn } from "@/lib/utils";
+import { format } from "date-fns";
 
 type WhatsappKanbanItemProps = DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
   isSelected: boolean;
@@ -23,35 +24,31 @@ export const WhatsappKanbanItem: FC<WhatsappKanbanItemProps> = ({
 
   return (
     <div
-      className={cn(
-        className,
-        "bg-background flex cursor-pointer justify-between gap-4 overflow-hidden border-b py-2 pr-2 hover:brightness-125",
-        {
-          "bg-primary/15": isSelected,
-        },
-      )}
+      className={cn(className, "bg-background cursor-pointer gap-4 overflow-hidden border-b py-2 pr-2 hover:brightness-125", {
+        "bg-primary/15": isSelected,
+      })}
       onClick={onClick}
       {...props}
     >
-      <div>
+      <div className="flex w-full items-center justify-between">
         <h3 className="font-bold">{contactMessage.surname || contactMessage.name}</h3>
-        <div className="flex flex-wrap gap-1">
-          {contactMessage.categories.map((category) => (
-            <Badge key={category.id}>{category.name}</Badge>
-          ))}
-        </div>
+        {contactMessage.isRead === false && <div className="bg-primary h-2 w-2 rounded-full"></div>}
+      </div>
+      <div className="flex flex-wrap gap-1">
+        {contactMessage.categories.map((category) => (
+          <Badge key={category.id}>{category.name}</Badge>
+        ))}
+      </div>
+      <div className="flex w-full justify-between">
         <WhatsappChatContactMessage isIncoming={isIncoming} messageContentType={contactMessage.messageContentType}>
           {contactMessage.messageContent}
         </WhatsappChatContactMessage>
-
-        {contactMessage.serviceUserServiceName && (
-          <p className="mt-3 text-xs">Em atendimento por: {contactMessage.serviceUserServiceName}</p>
-        )}
+        <span className="text-xs whitespace-nowrap">{format(contactMessage.messageCreatedAt, "dd/MM/yy HH:mm")}</span>
       </div>
 
-      <div className="grid h-full grid-rows-2">
-        {contactMessage.isRead === false && <div className="bg-primary h-2 w-2 self-start justify-self-end rounded-full"></div>}
-        <div className="row-start-2 flex flex-wrap justify-end gap-1 self-end justify-self-end align-bottom">
+      <div className="mt-3 grid grid-cols-[auto_44px]">
+        {contactMessage.serviceUserServiceName && <p className="text-xs">Em atendimento por: {contactMessage.serviceUserServiceName}</p>}
+        <div className="col-start-2 flex flex-wrap justify-end gap-1">
           {usersInContact.map((user) => (
             <div
               className="bg-secondary text-secondary-foreground flex h-5 w-5 items-center justify-center rounded-full text-xs uppercase"
