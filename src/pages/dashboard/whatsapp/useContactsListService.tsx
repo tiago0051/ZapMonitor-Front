15b6ad1713a1fed4a16f499866a953fd58e59b56
Tiago Salgado <tiago.salgado@devsoftbr.com>
2026-09-25 11:42:58 -0300

    Correção da adição de itens em tempo real na lista de fila de contatos

--- src/pages/dashboard/whatsapp/useContactsListService.tsx
+++ src/pages/dashboard/whatsapp/useContactsListService.tsx
@@ -96,6 +96,19 @@ export const useContactsListService = ({ search, tab }: UseContactsListService) 
           });
           return;
         }
+
+        const doAddContact =
+          contactsMessageQuery.data && !contactsMessageQuery.data?.canNextPage && contactsMessageQuery.data?.total >= takeItems;
+        const doAddContactInQueueTab = doAddContact && isTabQueue && !contact.serviceRepresentative;
+        const doAddContactInMineTab =
+          doAddContact && isTabMine && !!contact.serviceRepresentative && contact.serviceRepresentative === user?.name;
+
+        if (doAddContactInQueueTab || doAddContactInMineTab) {
+          queryClient.setQueryData(queryKey, {
+            ...old,
+            items: [...items, contact],
+          });
+        }
       });
     }
 
